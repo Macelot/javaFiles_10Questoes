d90953f55355fc9553ec6927ae62b76f83723490
marcelot <marcelojtelles@gmail.com>
2020-08-27 14:56:47 -0300

    Add files via upload

---
+++ Aula04/src/main/java/br/com/empresa/aula04/NewMain.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package br.com.empresa.telaaula4;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.FileWriter;
+
+/**
+ *
+ * @author marce
+ */
+public class NewMain {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        
+        Operacoes operacoes;
+        operacoes = new Operacoes();
+        
+        operacoes.q1();
+        operacoes.q2();
+        operacoes.q3();
+        operacoes.q4();
+        operacoes.q5();
+        operacoes.q6();
+        operacoes.q7();
+        operacoes.q8();
+        operacoes.q9();
+        operacoes.q10();
+
+    }
+    
+}
